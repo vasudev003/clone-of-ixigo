e5354a4f9bda9b366c93fa51c8b7b855ad42cc9c
vasudev003 <vasudevpeshwani.vp@gmail.com>
2023-02-06 18:37:57 +0530

    initial

---
+++ src/Components/LandingPage.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Navbar from "./Navbar";
+import ImageSlider from "./ImageSlider";
+import MiddlePart from "./MiddlePart";
+import Footer from "./Footer";
+
+const LandingPage = () => {
+    return (
+        <div>
+            <Navbar />
+            {/* <br /><br /><br /><br /><br /> */}
+            <ImageSlider />
+            <MiddlePart />
+            <Footer />
+        </div>
+    );
+};
+
+export default LandingPage;
---
+++ src/Components/MiddlePart.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import FlightApp from "./FlightApp";
+
+const MiddlePart = () => {
+  return (
+    <div>
+      <FlightApp />
+      <div style={{ clear: "both" }}></div>
+    </div>
+  );
+};
+
+export default MiddlePart;
